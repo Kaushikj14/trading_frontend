a7efbb36629074f5893e2af2e01a1106c4471cc7
Kaushikj14 <kaushikjain1423@gmail.com>
2025-01-05 16:13:20 +0530

    Wallet Component Completed

--- src/page/Wallet/TransferForm.jsx
+++ src/page/Wallet/TransferForm.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { DialogClose } from "@radix-ui/react-dialog";
+import React from "react";
+import { Input } from "@/components/ui/input";
+
 
 const TransferForm = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [formData, setformData] = React.useState({
+    amount: "",
+    walletId: "",
+    purpose: "",
+  });
 
-export default TransferForm
+  const handleChange = (e) => {
+    setformData({ ...formData, [e.target.name]: [e.target.value] });
+  };
+
+  const handleSubmit = () => {
+    console.log(formData);
+  };
+
+  return (
+    <div className="pt-10 space-y-5">
+      <div>
+        <h1 className="pb-1">Enter Amount</h1>
+        <Input
+          name="amount"
+          onChange={handleChange}
+          value={formData.amount}
+          className="py-7"
+          placeholder="$9999"
+        />
+      </div>
+
+      <div>
+        <h1 className="pb-1">Enter Wallet Id</h1>
+        <Input
+          name="walletId"
+          onChange={handleChange}
+          value={formData.walletId}
+          className="py-7"
+          placeholder="#ADZK"
+        />
+      </div>
+
+      <div>
+        <h1 className="pb-1">Enter Purpose</h1>
+        <Input
+          name="purpose"
+          onChange={handleChange}
+          value={formData.purpose}
+          className="py-7"
+          placeholder="gift for your frinend"
+        />
+      </div>
+
+      <DialogClose>
+        <Button className="w-full py-7" onClick={handleSubmit}>
+          Submit
+        </Button>
+      </DialogClose>
+    </div>
+  );
+};
+
+export default TransferForm;
--- src/page/Wallet/WihdrawalForm.jsx
+++ src/page/Wallet/WihdrawalForm.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import React from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { DialogClose } from "@radix-ui/react-dialog";
 
 const WihdrawalForm = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [amount, setAmount] = React.useState("");
 
-export default WihdrawalForm
+  const handleSubmit = (e) => {};
+
+  const handleChange = (e) => {
+    setAmount(e.target.value);
+  };
+
+  return (
+    <div className="pt-10  space-y-5">
+      <div className="flex justify-between items-center rounded-md bg-slate-800 text-xl font-bold px-5 py-4">
+        <p>Available Balance</p>
+        <p>$87989</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <h1>Enter Withdrawal amount</h1>
+        <div className="flex items-center justify-center">
+          <Input
+            onChange={handleChange}
+            value={amount}
+            className="WithdrwalInput py-7 border-none outline-none focus:outline-none px-0 text2xl text-center"
+            placeholder="$9999"
+            type="number"
+          />
+        </div>
+      </div>
+
+      <div>
+        <p className="pb-2">Transfer to</p>
+        <div className="flex items-center gap-5 border px-5 py-2 rounded-md">
+          <img
+            className="h-12 w-12"
+            src="https://happay.com/blog/wp-content/uploads/sites/12/2022/09/baas-banking-as-a-service-.png"
+            alt=""
+          />
+          <div>
+            <p className="text-xl font-bold"> Yes Bank</p>
+            <p className="text-xs">*******8833</p>
+          </div>
+        </div>
+      </div>
+      <DialogClose className="w-full">
+        <Button className="w-full py-7 text-xl" onClick={handleSubmit}>
+          Withdraw
+        </Button>
+      </DialogClose>
+    </div>
+  );
+};
+
+export default WihdrawalForm;
