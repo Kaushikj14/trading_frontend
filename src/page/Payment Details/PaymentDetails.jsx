07b84d3edd333ffd11a4e9a554652cc8530fbfdc
Kaushikj14 <kaushikjain1423@gmail.com>
2025-01-06 20:22:12 +0530

    Payment Details Component Completdd

--- src/page/Payment Details/PaymentDetails.jsx
+++ src/page/Payment Details/PaymentDetails.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import React from "react";
+import PaymentDetailsForm from "./PaymentDetailsForm";
 
 const PaymentDetails = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="px-20">
+      <h1 className="text-3xl font-bold py-10">Payment Details</h1>
+    {false?  <Card>
+        <CardHeader className="text-left">
+          <CardTitle>Yes Bank</CardTitle>
+          <CardDescription>A/C Number : ********8833</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="flex items-center text-left">
+            <p className="w-32">Account Holder</p>
+            <p className="text-gray-400"> : Demo</p>
+          </div>
 
-export default PaymentDetails
+          <div className="flex items-center text-left">
+            <p className="w-32">IFSC</p>
+            <p className="text-gray-400">: YES0000008</p>
+          </div>
+        </CardContent>
+      </Card>: <Dialog>
+        <DialogTrigger>
+          <Button className="py-6">Add Payment Details</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Payment Details</DialogTitle>
+           
+          </DialogHeader>
+          <PaymentDetailsForm />
+        </DialogContent>
+      </Dialog>}
+     
+    </div>
+  );
+};
+
+export default PaymentDetails;
